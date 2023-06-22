d81c23d81ade3706c00c1bc16dd5eccfe86c1806
donovan-cartier <donovan.cartier0@gmail.com>
2023-06-22 14:56:35 +0200

    Add VR support

--- Sawmus/Assets/SCRIPT_ENTITYSamus.cs
+++ Sawmus/Assets/SCRIPT_ENTITYSamus.cs
@@ -6,21 +6,29 @@ public class SCRIPT_ENTITYSamus : MonoBehaviour
 {
 
     Animator animator;
+    GameObject bowlContainer;
+    public GameObject bowlPrefab;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        bowlContainer = GameObject.Find("BowlContainer");
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        animator.SetBool("chasing", true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void DropBowl()
+    {
+        bowlContainer.SetActive(false);
+        Instantiate(bowlPrefab, new Vector3(-2.638f, 2.08999991f, -4.63999987f), Quaternion.identity);
     }
 }
