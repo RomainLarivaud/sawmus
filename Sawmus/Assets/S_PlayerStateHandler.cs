6814c5d45ab5b26227d3cda93c352bc81605ab14
donovan-cartier <donovan.cartier0@gmail.com>
2023-06-22 23:12:27 +0200

    On peut partir du spawn

--- Sawmus/Assets/S_PlayerStateHandler.cs
+++ Sawmus/Assets/S_PlayerStateHandler.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using BNG;
 using UnityEngine;
 
 public class S_PlayerStateHandler : MonoBehaviour
 {
     public bool canMove = false;
     BNG.SmoothLocomotion smoothLocomotion;
+    public VREmulator vrEmulator;
+    public Vector3 storedLeftHandPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         smoothLocomotion = GetComponent<BNG.SmoothLocomotion>();
+        vrEmulator = GetComponentInParent<VREmulator>();
         smoothLocomotion.UpdateMovement = canMove;
+
+        storedLeftHandPosition = vrEmulator.LeftControllerPosition;
+        vrEmulator.LeftControllerPosition = new Vector3(-8.36999989f, -0.200000003f, 0.5f);
     }
 
     // Update is called once per frame
@@ -20,7 +27,7 @@ public class S_PlayerStateHandler : MonoBehaviour
         
     }
 
-    void ToggleMovement()
+    public void ToggleMovement()
     {
         canMove = !canMove;
         smoothLocomotion.UpdateMovement = canMove;
